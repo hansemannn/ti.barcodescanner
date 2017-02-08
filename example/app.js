d0e041bb6f72203dd7e43726451e1f6facafa885
Hans Knöchel <hans.knoechel@hs-osnabrueck.de>
2017-02-08 20:38:51 +0100

    Add example

--- example/app.js
+++ example/app.js
@@ -1,39 +1,162 @@
-// This is a test harness for your module
-// You should do something interesting in this harness
-// to test out the module and to provide instructions
-// to users on how to use it by example.
+/**
+ * In this example, we'll use the Barcode module to display some information about
+ * the scanned barcode.
+ */
+var Barcode = require('ti.barcodescanner');
+Barcode.allowRotation = true;
+Barcode.useLED = true;
 
-
-// open a single window
-var win = Ti.UI.createWindow({
-	backgroundColor:'white'
+var window = Ti.UI.createWindow({
+    backgroundColor: 'white'
 });
-var label = Ti.UI.createLabel();
-win.add(label);
-win.open();
+var scrollView = Ti.UI.createScrollView({
+    contentWidth: 'auto',
+    contentHeight: 'auto',
+    top: 0,
+    showVerticalScrollIndicator: true,
+    layout: 'vertical'
+});
 
-// TODO: write your module tests here
-var ti_barcodescanner = require('ti.barcodescanner');
-Ti.API.info("module is => " + ti_barcodescanner);
+/**
+ * Create a chrome for the barcode scanner.
+ */
+var overlay = Ti.UI.createView({
+    backgroundColor: 'transparent',
+    top: 0, right: 0, bottom: 0, left: 0
+});
+var switchButton = Ti.UI.createButton({
+    title: Barcode.useFrontCamera ? 'Back Camera' : 'Front Camera',
+    textAlign: 'center',
+    color: '#000', backgroundColor: '#fff', style: 0,
+    font: { fontWeight: 'bold', fontSize: 16 },
+    borderColor: '#000', borderRadius: 10, borderWidth: 1,
+    opacity: 0.5,
+    width: 220, height: 30,
+    bottom: 10
+});
+switchButton.addEventListener('click', function () {
+    Barcode.useFrontCamera = !Barcode.useFrontCamera;
+    switchButton.title = Barcode.useFrontCamera ? 'Back Camera' : 'Front Camera';
+});
+overlay.add(switchButton);
 
-label.text = ti_barcodescanner.example();
+var toggleLEDButton = Ti.UI.createButton({
+    title: Barcode.useLED ? 'LED is On' : 'LED is Off',
+    textAlign: 'center',
+    color: '#000', backgroundColor: '#fff', style: 0,
+    font: { fontWeight: 'bold', fontSize: 16 },
+    borderColor: '#000', borderRadius: 10, borderWidth: 1,
+    opacity: 0.5,
+    width: 220, height: 30,
+    bottom: 40
+});
+toggleLEDButton.addEventListener('click', function () {
+    Barcode.useLED = !Barcode.useLED;
+    toggleLEDButton.title = Barcode.useLED ? 'LED is On' : 'LED is Off';
+});
+overlay.add(toggleLEDButton);
 
-Ti.API.info("module exampleProp is => " + ti_barcodescanner.exampleProp);
-ti_barcodescanner.exampleProp = "This is a test value";
+var cancelButton = Ti.UI.createButton({
+    title: 'Cancel', textAlign: 'center',
+    color: '#000', backgroundColor: '#fff', style: 0,
+    font: { fontWeight: 'bold', fontSize: 16 },
+    borderColor: '#000', borderRadius: 10, borderWidth: 1,
+    opacity: 0.5,
+    width: 220, height: 30,
+    top: 40
+});
+cancelButton.addEventListener('click', function () {
+    Barcode.cancel();
+});
+overlay.add(cancelButton);
 
-if (Ti.Platform.name == "android") {
-	var proxy = ti_barcodescanner.createExample({
-		message: "Creating an example Proxy",
-		backgroundColor: "red",
-		width: 100,
-		height: 100,
-		top: 100,
-		left: 150
-	});
+/**
+ * Create a button that will trigger the barcode scanner.
+ */
+var scanCode = Ti.UI.createButton({
+    title: 'Scan Code',
+    width: 200,
+    height: 60,
+    top: 20
+});
+scanCode.addEventListener('click', function () {
+    reset();
+    // Note: while the simulator will NOT show a camera stream in the simulator, you may still call "Barcode.capture"
+    // to test your barcode scanning overlay.
+    Barcode.capture({
+        animate: true,
+        overlay: overlay,
+        showCancel: false, // unused
+        showRectangle: false, // unused
+        keepOpen: true/*,
+        acceptedFormats: [
+            Barcode.FORMAT_QR_CODE // default: All codes
+        ]*/
+    });
+});
+scrollView.add(scanCode);
 
-	proxy.printMessage("Hello world!");
-	proxy.message = "Hi world!.  It's me again.";
-	proxy.printMessage("Hello world!");
-	win.add(proxy);
+/**
+ * Now listen for various events from the Barcode module. This is the module's way of communicating with us.
+ */
+var scannedBarcodes = {}, scannedBarcodesCount = 0;
+function reset() {
+    scannedBarcodes = {};
+    scannedBarcodesCount = 0;
+    cancelButton.title = 'Cancel';
+
+    scanResult.text = ' ';
+    scanContentType.text = ' ';
 }
+Barcode.addEventListener('error', function (e) {
+    scanContentType.text = ' ';
+    scanResult.text = e.message;
+});
+Barcode.addEventListener('cancel', function (e) {
+    Ti.API.info('Cancel received');
+});
+Barcode.addEventListener('success', function (e) {
+    Ti.API.info('Success called with barcode: ' + e.result);
+    if (!scannedBarcodes['' + e.result]) {
+        scannedBarcodes[e.result] = true;
+        scannedBarcodesCount += 1;
+        cancelButton.title = 'Finished (' + scannedBarcodesCount + ' Scanned)';
 
+        scanResult.text += e.result + ' ';
+    }
+});
+
+/**
+ * Finally, we'll add a couple labels to the window. When the user scans a barcode, we'll stick information about it in
+ * to these labels.
+ */
+scrollView.add(Ti.UI.createLabel({
+    text: 'You may need to rotate the device',
+    top: 10,
+    height: Ti.UI.SIZE, 
+	width: Ti.UI.SIZE
+}));
+
+scrollView.add(Ti.UI.createLabel({
+    text: 'Result: ', textAlign: 'left',
+    top: 10, left: 10,
+    color: 'black',
+    height: Ti.UI.SIZE || 'auto'
+}));
+var scanResult = Ti.UI.createLabel({
+    text: ' ', textAlign: 'left',
+    top: 10, left: 10,
+    color: 'black',
+    height: Ti.UI.SIZE || 'auto'
+});
+scrollView.add(scanResult);
+
+scrollView.add(Ti.UI.createLabel({
+    text: 'Parsed: ', textAlign: 'left',
+    top: 10, left: 10,
+    color: 'black',
+    height: Ti.UI.SIZE || 'auto'
+}));
+
+window.add(scrollView);
+window.open();
